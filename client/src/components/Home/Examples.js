a0229a5b0df463b6bd220f6547881a8863967865
vdumic <vdumic00@fesb.hr>
2024-06-27 16:40:49 +0200

    Home page text and pictures changed

--- client/src/components/Home/Examples.js
+++ client/src/components/Home/Examples.js
@@ -1,31 +1,45 @@
-import board from "../../assets/board_example.png";
+import projectsPage from "../../assets/projects_page.png";
+import projectBoard from "../../assets/project_board.png";
 
 const Examples = () => {
   return (
     <div className="bg-bckgrnd-main container mx-auto my-14">
       <div className="flex items-center justify-between mx-40 pl-14">
         <div className="mx-24">
-          <h1 className="text-text-dark text-3xl py-4 text-center break-normal">
-            Example project #1
+          <h1 className="text-text-dark font-medium text-2xl py-4 text-center break-normal">
+            Your Projects, All in One Place
           </h1>
-          <p className="text-text-dark font-normal text-lg break-normal">
-            Organize your project <br />
-            Create custom task status <br />
-            Add your colleagues to project
+          <p className="text-text-dark font-normal text-lg break-normal text-center">
+            Stay organized and in control with our intuitive project management
+            app.
+          </p>
+          <p className="text-text-dark font-normal text-lg break-normal text-center">
+            View all your projects at a glance, track progress, and manage tasks
+            effortlessly.
           </p>
         </div>
-        <img src={board} alt="Sprynt application hero section" width="550" />
+        <img
+          src={projectsPage}
+          alt="Sprynt application hero section"
+          width="550"
+        />
       </div>
       <div className="flex items-center justify-between mx-40 mt-10 pr-14">
-        <img src={board} alt="Sprynt application hero section" width="550" />
+        <img
+          src={projectBoard}
+          alt="Sprynt application hero section"
+          width="550"
+        />
         <div className="mx-24">
-          <h1 className="text-text-dark text-3xl py-4 text-center break-normal">
-            Example project #2
+          <h1 className="text-text-dark font-medium text-2xl py-4 text-center break-normal">
+            Manage Your Tasks Effortlessly
           </h1>
-          <p className="text-text-dark font-normal text-lg break-normal">
-            Organize your project <br />
-            Create custom task status <br />
-            Add your colleagues to project
+          <p className="text-text-dark font-normal text-lg break-normal text-center">
+            Stay on top of your projects with our powerful task management
+            features.
+          </p>
+          <p className="text-text-dark font-normal text-lg break-normal text-center">
+            Organize, prioritize, and track your tasks all in one place.
           </p>
         </div>
       </div>
