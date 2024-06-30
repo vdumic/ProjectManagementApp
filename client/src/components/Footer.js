baa0d46c4267bb1418742a4b38d38a6f23831d9c
vdumic <vdumic00@fesb.hr>
2024-06-30 12:23:50 +0200

    Links added to footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
 import { SocialIcon } from "react-social-icons";
 
@@ -8,28 +9,30 @@ const Footer = () => {
         <div className="flex items-center justify-between flex-wrap py-10 px-20">
           <div className="flex justify-items-center items-center w-full sm:w-2/3 lg:w-3/12">
             <div className="w-full flex items-center justify-center">
-              <img src={logo} alt="Sprynt logo" height="100" width="150" />
+              <Link to="/">
+                <img src={logo} alt="Sprynt logo" height="100" width="150" />
+              </Link>
             </div>
           </div>
           <div className="w-full align-middle sm:w-1/2 lg:w-2/12">
             <div className="my-auto w-full">
               <ul>
                 <li className="flex items-center justify-center">
-                  <button>
+                  <Link to="/work-management">
                     <p className="text-text-dark text-lg py-2 px-1">
                       Work management
                     </p>
-                  </button>
+                  </Link>
                 </li>
                 <li className="flex items-center justify-center">
-                  <button>
+                  <Link to="/about-us">
                     <p className="text-text-dark text-lg py-2 px-1">About us</p>
-                  </button>
+                  </Link>
                 </li>
                 <li className="flex items-center justify-center">
-                  <button>
+                  <Link to="/support">
                     <p className="text-text-dark text-lg py-2 px-1">Support</p>
-                  </button>
+                  </Link>
                 </li>
               </ul>
             </div>
